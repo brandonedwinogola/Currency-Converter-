dddae9bfdf0cfe10913f8e0f9b133e4073ba7418
Brandon Edwin Ogola <edogola4@gmail.com>
2024-11-18 10:02:55 +0300

    Update currency-convertor.jsx

--- currency-converter/src/components/currency-convertor.jsx
+++ currency-converter/src/components/currency-convertor.jsx
@@ -123,7 +123,7 @@ const CurrencyConverter = () => {
           className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
           ${converting ? "animate-pulse" : ""}`}
         >
-          Convert
+          Convert..
         </button>
       </div>
 
